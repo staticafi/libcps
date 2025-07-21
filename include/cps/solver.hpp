d8fd2e327e0bf6f58c62fbcc04e6f2143909dbeb
Marek Trtik <trtikm@gmail.com>
2025-07-21 23:30:34 +0200

    Sketch of API of the solver.

--- include/cps/solver.hpp
+++ include/cps/solver.hpp
@@ -1,7 +1,99 @@
 #ifndef CPS_SOLVER_HPP_INCLUDED
 #   define CPS_SOLVER_HPP_INCLUDED
 
+#   include <vector>
+#   include <functional>
+#   include <cstdint>
+
 namespace cps {
+
+
+using Valuation = std::vector<std::uint8_t>;
+
+
+struct Variable
+{
+    enum struct Type : std::uint8_t
+    {
+        BOOLEAN = 0U,
+
+        UINT8 = 1U,
+        SINT8 = 2U,
+
+        UINT16 = 3U,
+        SINT16 = 4U,
+
+        UINT32 = 5U,
+        SINT32 = 6U,
+
+        UINT64 = 7U,
+        SINT64 = 8U,
+
+        FLOAT32 = 9U,
+        FLOAT64 = 10U,
+    };
+
+    std::size_t start_byte_index;
+    Type type;
+};
+
+
+enum struct Comparator : std::uint8_t
+{
+    EQUAL           = 0,
+    UNEQUAL         = 1,
+    LESS            = 2,
+    LESS_EQUAL      = 3,
+    GREATER         = 4,
+    GREATER_EQUAL   = 5
+};
+
+
+struct Setup
+{
+    std::vector<Variable> variables;
+    std::vector<std::vector<std::size_t> > parameter_indices;
+    std::vector<Comparator> comparators;
+};
+
+
+using Real = double;
+
+
+struct Result
+{
+    bool valid() const { return !predicate_values.empty() && predicate_values.size() == bb_functions_values.size(); }
+    std::vector<Real> bb_functions_values{};
+    std::vector<bool> predicate_values{};
+};
+
+
+struct Solver
+{
+    Solver(Setup const& setup_, Valuation const& seed_valuation_, Result const& seed_result_);
+
+    Setup const& get_setup() const { return setup; }
+
+    bool is_finished() const { return finished; }
+    bool is_solution(Result const& result) const;
+
+    void compute_next_valuation(Valuation& valuation);
+    void process_result(Result const& result);
+
+private:
+    Setup setup;
+    Valuation seed_valuation;
+    Result seed_result;
+    bool finished;
+};
+
+
+Result solve(
+    Setup const& setup,
+    Valuation const& seed_valuation,
+    Result const& seed_result,
+    std::function<Result(Valuation const&, Setup const&)> const& evaluator
+    );
 
 
 }
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,6 +1,67 @@
 #include <cps/solver.hpp>
+#include <utility/assumptions.hpp>
 
 namespace cps {
+
+
+Solver::Solver(Setup const& setup_, Valuation const& seed_valuation_, Result const& seed_result_)
+    : setup{ setup_ }
+    , seed_valuation{ seed_valuation_ }
+    , seed_result{ seed_result_ }
+    , finished{ false }
+{
+    ASSUMPTION(
+        !setup.variables.empty() &&
+        !setup.comparators.empty() &&
+        setup.comparators.size() == setup.parameter_indices.size() &&
+        !setup.parameter_indices.back().empty()
+    );
+}
+
+
+bool Solver::is_solution(Result const& result) const
+{
+    if (!result.valid() || result.predicate_values.size() < seed_result.predicate_values.size())
+        return false;
+    std::size_t const last{ seed_result.predicate_values.size() - 1ULL };
+    for (std::size_t i{ 0ULL }; i != last; ++i)
+        if (result.predicate_values.at(i) != seed_result.predicate_values.at(i))
+            return false;
+    return result.predicate_values.at(last) != seed_result.predicate_values.at(last);
+}
+
+
+void Solver::compute_next_valuation(Valuation& valuation)
+{
+    valuation = seed_valuation;
+}
+
+
+void Solver::process_result(Result const& result)
+{
+    finished = true; // TODO: This is incorrect temporary implementation.
+}
+
+
+Result solve(
+    Setup const& setup,
+    Valuation const& seed_valuation,
+    Result const& seed_result,
+    std::function<Result(Valuation const&, Setup const&)> const& evaluator
+    )
+{
+    Solver solver{ setup, seed_valuation, seed_result };
+    Valuation valuation;
+    while (!solver.is_finished())
+    {
+        solver.compute_next_valuation(valuation);
+        Result result{ evaluator(valuation, solver.get_setup()) };
+        if (solver.is_solution(result))
+            return result;
+        solver.process_result(result);
+    }
+    return {};
+}
 
 
 }
