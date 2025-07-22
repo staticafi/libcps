90c370d9435330bd730c788ae8cfca32530458f5
Marek Trtik <trtikm@gmail.com>
2025-07-23 00:06:54 +0200

    Added thin wrapper for Eigen vector and matrix.

---
+++ include/cps/math.hpp
@@ -0,0 +1,15 @@
+#ifndef CPS_MATH_HPP_INCLUDED
+#   define CPS_MATH_HPP_INCLUDED
+
+#   include <Eigen/Dense>
+
+namespace cps {
+
+
+using Vector = Eigen::VectorXd;
+using Matrix = Eigen::MatrixXd;
+
+
+}
+
+#endif
